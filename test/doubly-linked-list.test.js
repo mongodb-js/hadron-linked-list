33af133f108d2cf5b560bd1c072111dd4182cc5a
Durran Jordan <durran@gmail.com>
2016-07-04 21:15:13 +0200

    Append the first data to the doubly linked list

--- test/doubly-linked-list.test.js
+++ test/doubly-linked-list.test.js
@@ -5,6 +5,41 @@ const expect = chai.expect;
 const List = require('../lib/doubly-linked-list.js');
 
 describe('DoublyLinkedList', function() {
+  describe('#append', function() {
+    context('when the data is the first in the list', function() {
+      var list = new List();
+      var node = list.append('first');
+
+      it('returns the node', function() {
+        expect(node.data).to.equal('first');
+      });
+
+      it('sets the first node to the data node', function() {
+        expect(list.firstNode).to.equal(node);
+      });
+
+      it('sets the last node to the data node', function() {
+        expect(list.lastNode).to.equal(node);
+      });
+
+      it('sets the node data previous node to null', function() {
+        expect(node.previousNode).to.equal(null);
+      });
+
+      it('sets the node data next node to null', function() {
+        expect(node.nextNode).to.equal(null);
+      });
+
+      it('sets the size of the list to 1', function() {
+        expect(list.size).to.equal(1);
+      });
+    });
+
+    context('when the data is not the first in the list', function() {
+
+    });
+  });
+
   describe('#constructor', function() {
     context('when no arguments are passed', function() {
       var list = new List();
